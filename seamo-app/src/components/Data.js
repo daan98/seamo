0583800634e625eabbc76f07a47ca6c697d0b4fd
daan98 <daan98.da@gmail.com>
2022-01-25 20:23:10 -0600

    Movie page carousel slider working. Movie information filled

--- seamo-app/src/components/Data.js
+++ seamo-app/src/components/Data.js
@@ -1,15 +1,56 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faUser, faVideo,  } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import comedyImg from '../assets/comedy/comedy.jpg';
 import actionImg from '../assets/action/action.png';
+import isSpartar from '../assets/action/300.png';
+import deadpool from '../assets/action/deadpool.png';
+import endGame from '../assets/action/endgame.png';
+import fastEight from '../assets/action/fast-furious-8.png';
 import johnWick from '../assets/action/john-wick-3.png'
+import superman from '../assets/action/man-steel.png';
+import matrix from '../assets/action/matrix.png';
+import comedyImg from '../assets/comedy/comedy.jpg';
+import grownUp from '../assets/comedy/grown-ups.png';
+import littleMan from '../assets/comedy/little-man.png';
+import norbit from '../assets/comedy/norbit.png';
+import vacation from '../assets/comedy/vacation.png';
+import theMillers from '../assets/comedy/were-the-millers.png';
 import docuImg from '../assets/documentary/documentary.png';
+import abducted from '../assets/documentary/abducted.png';
+import lilPeep from '../assets/documentary/lil-peep.png';
+import rousey from '../assets/documentary/ronda-rousey.png';
+import sepultura from '../assets/documentary/sepultura-endurance.png';
+import superSize from '../assets/documentary/super-size-me.png';
 import dramaImg from '../assets/drama/drama.jpg';
-import kidImg from '../assets/kid/kid.jpg'
-import romanceImg from '../assets/romance/romance.png';
+import collaBeuaty from '../assets/drama/collateral-beauty.png';
+import concussion from '../assets/drama/concussion.png';
+import creedII from '../assets/drama/creed-2.png';
+import sevenPounds from '../assets/drama/seven-pounds.png';
+import impossible from '../assets/drama/the-impossible.png';
 import horrorImg from '../assets/horror/horror.png';
-import vintageImg from '../assets/vintage/vintage.png'
+import quietPlaceII from '../assets/horror/a-quiet-place-2.png';
+import deadSilence from '../assets/horror/dead-silence.png';
+import itII from '../assets/horror/it-2.png';
+import markedOnes from '../assets/horror/paranormal-activity-marked-ones.png';
+import witch from '../assets/horror/the-witch.png';
+import kidImg from '../assets/kid/kid.jpg';
+import dorado from '../assets/kid/el-dorado.png';
+import trainDragonIII from '../assets/kid/how-to-train-your-dragon-3.png';
+import shrekII from '../assets/kid/shrek-2.png';
+import sinbad from '../assets/kid/sinbad.png';
+import croods from '../assets/kid/the-croods.png';
+import romanceImg from '../assets/romance/romance.png';
+import time from '../assets/romance/about-time.png';
+import hitch from '../assets/romance/hitch.png';
+import longRide from '../assets/romance/the-longest-ride.png';
+import vow from '../assets/romance/the-vow.png';
+import yourName from '../assets/romance/your-name.png';
+import vintageImg from '../assets/vintage/vintage.png';
+import kane from '../assets/vintage/citizen-kane.png';
+import enterDragon from '../assets/vintage/enter-the-dragon.png';
+import godFather from '../assets/vintage/godfather.png';
+import psycho from '../assets/vintage/psycho.png';
+import ozWizard from '../assets/vintage/the-wizard-of-oz.png';
 
 export const getNavbarLink = [
     {
@@ -55,171 +96,213 @@ export const getSocialLink = [
 export const getMoviesImage = [
     {
         id: 1,
-        url: '../assets/action/300.png',
+        url: isSpartar,
+        genre: ["action"],
     },
     {
         id: 2,
-        url: '../assets/action/deadpool.png',
+        url: deadpool,
+        genre: ["action", "comedy"],
     },
     {
         id: 3,
-        url: '../assets/action/endgame.png',
+        url: endGame,
+        genre: ["action", "comedy", "drama"],
     },
     {
         id: 4,
-        url: '../assets/action/fast-furious-8.png',
+        url: fastEight,
+        genre: ["action", "comedy", "drama"],
     },
     {
         id: 5,
-        url: '../assets/action/john-wick-3.png',
+        url: johnWick,
+        genre: ["action"],
     },
     {
         id: 6,
-        url: '../assets/man-steel.png',
+        url: superman,
+        genre: ["action", "sci-fi"],
     },
     {
         id: 7,
-        url: '../assets/action/matrix.png',
+        url: matrix,
+        genre: ["action", "sci-fi", "romance"],
     },
     {
         id: 8,
-        url: '../assets/comedy/grown-ups.png',
+        url: grownUp,
+        genre: ["comedy"],
     },
     {
         id: 9,
-        url: '../assets/comedy/little-man.png',
+        url: littleMan,
+        genre: ["comedy"],
     },
     {
         id: 10,
-        url: '../assets/comedy/norbit.png',
+        url: norbit,
+        genre: ["comedy"],
     },
     {
         id: 11,
-        url: '../assets/comedy/vacation.png',
+        url: vacation,
+        genre: ["comedy"],
     },
     {
         id: 12,
-        url: '../assets/comedy/were-the-millers.png',
+        url: theMillers,
+        genre: ["comedy"],
     },
     {
         id: 13,
-        url: '../assets/documentary/abducted.png',
+        url: abducted,
+        genre: ["documentary"],
     },
     {
         id: 14,
-        url: '../assets/documentary/lil-peep.png',
+        url: lilPeep,
+        genre: ["documentary"],
     },
     {
         id: 15,
-        url: '../assets/documentary/ronda-rousey.png',
+        url: rousey,
+        genre: ["documentary"],
     },
     {
         id: 16,
-        url: '../assets/documentary/sepultura-endurance.png',
+        url: sepultura,
+        genre: ["documentary"],
     },
     {
         id: 17,
-        url: '../assets/documentary/super-size-me.png',
+        url: superSize,
+        genre: ["documentary"],
     },
     {
         id: 18,
-        url: '../assets/drama/collateral-beauty.png',
+        url: collaBeuaty,
+        genre: ["drama"],
     },
     {
         id: 19,
-        url: '../assets/drama/concussion.png',
+        url: concussion,
+        genre: ["drama"],
     },
     {
         id: 20,
-        url: '../assets/drama/creed-2.png',
+        url: creedII,
+        genre: ["drama", "comedy"],
     },
     {
         id: 21,
-        url: '../assets/drama/seven-pounds.png',
+        url: sevenPounds,
+        genre: ["drama"],
     },
     {
         id: 22,
-        url: '../assets/drama/the-impossible.png',
+        url: impossible,
+        genre: ["drama"],
     },
     {
         id: 23,
-        url: '../assets/horror/a-quiet-place-2.png',
+        url: quietPlaceII,
+        genre: ["horror", "suspense", "sci-fi"],
     },
     {
         id: 24,
-        url: '../assets/horror/dead-silence.png',
+        url: deadSilence,
+        genre: ["horror", "suspense"],
     },
     {
         id: 25,
-        url: '../assets/horror/it-2.png',
+        url: itII,
+        genre: ["horror", "suspense"],
     },
     {
         id: 26,
-        url: '../assets/horror/paranormal-activity-marked-ones.png',
+        url: markedOnes,
+        genre: ["horror", "suspense"],
     },
     {
         id: 27,
-        url: '../assets/horror/the-witch.png',
+        url: witch,
+        genre: ["horror", "suspense"],
     },
     {
         id: 28,
-        url: '../assets/kid/el-dorado.png',
+        url: dorado,
+        genre: ["kid", "animation", "action", "history"],
     },
     {
         id: 29,
-        url: '../assets/kid/how-to-train-your-dragon-3.png',
+        url: trainDragonIII,
+        genre: ["kid", "animation", "action", "history"],
     },
     {
         id: 30,
-        url: '../assets/kid/shrek-2.png',
+        url: shrekII,
+        genre: ["kid", "animation", "action", "comedy"],
     },
     {
         id: 31,
-        url: '../assets/kid/sinbad.png',
+        url: sinbad,
+        genre: ["kid", "animation", "action"],
     },
     {
         id: 32,
-        url: '../assets/kid/the-croods.png',
+        url: croods,
+        genre: ["kid", "animation", "action"],
     },
     {
         id: 33,
-        url: '../assets/romance/about-time.png',
+        url: time,
+        genre: ["romance", "comedy", "sci-fi", "drama"],
     },
     {
         id: 34,
-        url: '../assets/romance/hitch.png',
+        url: hitch,
+        genre: ["romance", "comedy", "drama"],
     },
     {
         id: 35,
-        url: '../assets/romance/the-longest-ride.png',
+        url: longRide,
+        genre: ["romance", "drama"],
     },
     {
         id: 36,
-        url: '../assets/romance/the-vow.png',
+        url: vow,
+        genre: ["romance", "drama"],
     },
     {
         id: 37,
-        url: '../assets/romance/your-name.png',
+        url: yourName,
+        genre: ["romance", "drama", "anime"],
     },
     {
         id: 38,
-        url: '../assets/vintage/citizen-kene.png',
+        url: kane,
+        genre: ["vintage", "drama"],
     },
     {
         id: 39,
-        url: '../assets/vintage/enter-the-dragon.png',
+        url: enterDragon,
+        genre: ["vintage", "action", "drama"],
     },
     {
         id: 40,
-        url: '../assets/vintage/godfather.png',
+        url: godFather,
+        genre: ["vintage", "action", "drama"],
     },
     {
         id: 41,
-        url: '../assets/vintage/psycho.png',
+        url: psycho,
+        genre: ["vintage", "horror", "drama"],
     },
     {
         id: 42,
-        url: '../assets/vintage/the-wizard-of-oz.png',
+        url: ozWizard,
+        genre: ["vintage", "sci-fi", "musical"],
     },
 ];
 
--- seamo-app/src/components/Movies.js
+++ seamo-app/src/components/Movies.js
@@ -12,19 +12,39 @@ function Movies(){
             items: 1,
         }
     }
+
+    const secondaryResponsive = {
+        superLarge: {
+            breakpoint: { max: 4000, min: 3001 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1025},
+            items: 4
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 768 },
+            items: 3
+        },
+        mobile: {
+            breakpoint: { max: 767, min: 0 },
+            items: 2
+        }
+    };
+
     return(
         <div>
             <Header />
             <div className="background">
                 <div className="movie-container container">
-                    <h1 className="title center yellow">Added Recently</h1>
+                    <h1 className="title yellow" style={{margin: "0.67em auto"}} >Added Recently</h1>
                     <Carousel 
                     responsive={mainResponsive}
                     swipeable={false}
                     draggable={false}
                     showDots={true}
                     infinite={true}
-                    autoPlay={false}
+                    autoPlay={true}
                     autoPlaySpeed={90000}
                     keyBoardControl={true}
                     >
@@ -53,8 +73,98 @@ function Movies(){
                             <p className="yellow"><span>John Wick: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla leo ligula, dapibus a iaculis sit amet, rutrum et tellus. Nullam sed odio vestibulum, scelerisque tellus mollis, consequat mi. Nullam eget blandit sem, nec porttitor urna. Etiam mattis lorem ut dolor ornare, ut consectetur justo volutpat. Phasellus vitae imperdiet turpis.</p>
                         </div>
                     </Carousel>
+
                     <h1 className="title yellow">Trend</h1>
+                    <Carousel
+                    responsive={secondaryResponsive}
+                    swipeable={false}
+                    draggable={false}
+                    showDots={true}
+                    infinite={true}
+                    autoPlay={false}
+                    autoPlaySpeed={90000}
+                    keyBoardControl={true}
+                    className="carousel-container"
+                    >
+                        <div className="carousel-item">
+                            <div className="image-container">
+                                <img src={getUserInfo.img} alt="TEST" />
+                            </div>
+                            <div className="movie-info-container">
+                                <p className="yellow"><span>John Wick: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla leo ligula, dapibus a iaculis sit amet, rutrum et tellus. Nullam sed odio vestibulum, scelerisque tellus mollis, consequat mi. Nullam eget blandit sem, nec porttitor urna. Etiam mattis lorem ut dolor ornare, ut consectetur justo volutpat. Phasellus vitae imperdiet turpis.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <div className="image-container">
+                                <img src={getUserInfo.img} alt="TEST" />
+                            </div>
+                            <div className="movie-info-container">
+                                <p className="yellow"><span>John Wick: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla leo ligula, dapibus a iaculis sit amet, rutrum et tellus. Nullam sed odio vestibulum, scelerisque tellus mollis, consequat mi. Nullam eget blandit sem, nec porttitor urna. Etiam mattis lorem ut dolor ornare, ut consectetur justo volutpat. Phasellus vitae imperdiet turpis.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <div className="image-container">
+                                <img src={getUserInfo.img} alt="TEST" />
+                            </div>
+                            <div className="movie-info-container">
+                                <p className="yellow"><span>John Wick: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla leo ligula, dapibus a iaculis sit amet, rutrum et tellus. Nullam sed odio vestibulum, scelerisque tellus mollis, consequat mi. Nullam eget blandit sem, nec porttitor urna. Etiam mattis lorem ut dolor ornare, ut consectetur justo volutpat. Phasellus vitae imperdiet turpis.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <div className="image-container">
+                                <img src={getUserInfo.img} alt="TEST" />
+                            </div>
+                            <div className="movie-info-container">
+                                <p className="yellow"><span>John Wick: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla leo ligula, dapibus a iaculis sit amet, rutrum et tellus. Nullam sed odio vestibulum, scelerisque tellus mollis, consequat mi. Nullam eget blandit sem, nec porttitor urna. Etiam mattis lorem ut dolor ornare, ut consectetur justo volutpat. Phasellus vitae imperdiet turpis.</p>
+                            </div>
+                        </div>
+                    </Carousel>
+
                     <h1 className="title yellow">For You</h1>
+                    <Carousel
+                    responsive={secondaryResponsive}
+                    swipeable={false}
+                    draggable={false}
+                    showDots={true}
+                    infinite={true}
+                    autoPlay={false}
+                    autoPlaySpeed={90000}
+                    keyBoardControl={true}
+                    className="carousel-container"
+                    >
+                        <div className="carousel-item">
+                            <div className="image-container">
+                                <img src={getUserInfo.img} alt="TEST" />
+                            </div>
+                            <div className="movie-info-container">
+                                <p className="yellow"><span>John Wick: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla leo ligula, dapibus a iaculis sit amet, rutrum et tellus. Nullam sed odio vestibulum, scelerisque tellus mollis, consequat mi. Nullam eget blandit sem, nec porttitor urna. Etiam mattis lorem ut dolor ornare, ut consectetur justo volutpat. Phasellus vitae imperdiet turpis.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <div className="image-container">
+                                <img src={getUserInfo.img} alt="TEST" />
+                            </div>
+                            <div className="movie-info-container">
+                                <p className="yellow"><span>John Wick: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla leo ligula, dapibus a iaculis sit amet, rutrum et tellus. Nullam sed odio vestibulum, scelerisque tellus mollis, consequat mi. Nullam eget blandit sem, nec porttitor urna. Etiam mattis lorem ut dolor ornare, ut consectetur justo volutpat. Phasellus vitae imperdiet turpis.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <div className="image-container">
+                                <img src={getUserInfo.img} alt="TEST" />
+                            </div>
+                            <div className="movie-info-container">
+                                <p className="yellow"><span>John Wick: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla leo ligula, dapibus a iaculis sit amet, rutrum et tellus. Nullam sed odio vestibulum, scelerisque tellus mollis, consequat mi. Nullam eget blandit sem, nec porttitor urna. Etiam mattis lorem ut dolor ornare, ut consectetur justo volutpat. Phasellus vitae imperdiet turpis.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <div className="image-container">
+                                <img src={getUserInfo.img} alt="TEST" />
+                            </div>
+                            <div className="movie-info-container">
+                                <p className="yellow"><span>John Wick: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla leo ligula, dapibus a iaculis sit amet, rutrum et tellus. Nullam sed odio vestibulum, scelerisque tellus mollis, consequat mi. Nullam eget blandit sem, nec porttitor urna. Etiam mattis lorem ut dolor ornare, ut consectetur justo volutpat. Phasellus vitae imperdiet turpis.</p>
+                            </div>
+                        </div>
+                    </Carousel>
                 </div>
             </div>
             <Footer />
