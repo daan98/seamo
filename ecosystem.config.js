97afb07a77e1bf47b5e4b9e383eccb04762fe7c4
daan98 <daan98.da@gmail.com>
2023-07-18 15:52:17 -0600

    main file name changed to index.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  apps : [{
-  name: "seamo-server",
-  script: "server.js",
-  instances: 1,
-  env: {
-  NODE_ENV: "development",
-  },
-  env_production: {
-  NODE_ENV: "production",
-  }
-  }]
-  };
+  apps: [
+    {
+      name: "seamo-server",
+      script: "index.js",
+      instances: 1,
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
