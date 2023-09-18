9068d9000fc4c6424534b50bc9d147e36f2e40db
daan98 <daan98.da@gmail.com>
2023-09-17 19:38:11 -0600

    add scroll to top to items in genre page

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -1,48 +1,71 @@
-import './Search.css'
-import './Genre.css'
+import "./Search.css";
+import "./Genre.css";
 import React from "react";
-import Header from './Header';
-import Footer from './Footer';
-import { play, info } from './Data';
-import { NavLink, useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux';
+import Header from "./Header";
+import Footer from "./Footer";
+import { play, info } from "./Data";
+import { NavLink, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-function Genre(props){
-    const params = useParams();
-    const movies = useSelector(state => state.movies);
+function Genre(props) {
+  const params = useParams();
+  const movies = useSelector((state) => state.movies);
 
-    const infoMovie = (e, link, linkLang) => {
-        if(e.target.nearestViewportElement.className.animVal.includes('info')) window.open(`https://${linkLang}.wikipedia.org/wiki/${link}`)
-    };
+  const infoMovie = (e, link, linkLang) => {
+    if (e.target.nearestViewportElement.className.animVal.includes("info"))
+      window.open(`https://${linkLang}.wikipedia.org/wiki/${link}`);
+  };
 
-    return(
-        <div id="main-element">
-            <Header />
-            <div className="background">
-                <div className="container">
-                    <h1 className="title yellow border">{params.id}</h1>
-                    <div className="genre-container">
-                        {movies.map(movie => {
-                            if(movie.genre.includes(params.id.toLowerCase())){
-                                const path = `/movie/${movie._id}`;
-                                return(
-                                    <div key={movie._id} className="genre-info">
-                                        <img src={movie.url} alt={movie.name} />
-                                        <div className="synopsis show-search">
-                                            <p className="yellow preview-info"><span>{movie.name} </span>{movie.bitDescription !== '-' ? movie.bitDescription : movie.description}</p>
-                                            <NavLink id='play' to={path} className="yellow movie-icon">{play}</NavLink>
-                                            <p id="info" className="yellow movie-icon" onClick={(e) => infoMovie(e, movie.wiki, movie.wikiLang)}>{info}</p>
-                                        </div>
-                                    </div>
-                                );
-                            } else{ return( true )}
-                        })}
+  return (
+    <div id="main-element">
+      <Header />
+      <div className="background">
+        <div className="container">
+          <h1 className="title yellow border">{params.id}</h1>
+          <div className="genre-container">
+            {movies.map((movie) => {
+              if (movie.genre.includes(params.id.toLowerCase())) {
+                const path = `/movie/${movie._id}`;
+                return (
+                  <div key={movie._id} className="genre-info">
+                    <img src={movie.url} alt={movie.name} />
+                    <div className="synopsis show-search">
+                      <p className="yellow preview-info">
+                        <span>{movie.name} </span>
+                        {movie.bitDescription !== "-"
+                          ? movie.bitDescription
+                          : movie.description}
+                      </p>
+                      <NavLink
+                        id="play"
+                        to={path}
+                        className="yellow movie-icon"
+                        onClick={(e) => window.scrollTo(0, 0)}
+                      >
+                        {play}
+                      </NavLink>
+                      <p
+                        id="info"
+                        className="yellow movie-icon"
+                        onClick={(e) =>
+                          infoMovie(e, movie.wiki, movie.wikiLang)
+                        }
+                      >
+                        {info}
+                      </p>
                     </div>
-                </div>
-            </div>
-            <Footer />
+                  </div>
+                );
+              } else {
+                return true;
+              }
+            })}
+          </div>
         </div>
-    );
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Genre;
